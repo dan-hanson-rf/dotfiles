61c35058aa86fbbff1635c036b24e0fe33e2ef4b
Dan Hanson <dan.hanson@mastercard.com>
2026-03-17 16:17:18 -0600

    ssh copy/paste config for nvim and tmux

--- .config/nvim/lua/config/init.lua
+++ .config/nvim/lua/config/init.lua
@@ -8,6 +8,18 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   command = [[%s/\s\+$//e]],
 })
 
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy '+',
+    --['*'] = require('vim.ui.clipboard.osc52').copy '*',
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste '+',
+    --['*'] = require('vim.ui.clipboard.osc52').paste '*',
+  },
+}
+
 -- lsp/lsp-zero
 local lsp_zero = require('lsp-zero')
 
